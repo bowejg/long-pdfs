91cdf031d8df406ec37f9156d28f0955239fb960
Ubuntu <ubuntu@ip-172-31-62-93.ec2.internal>
2021-02-28 17:18:31 +0000

    vis

--- command.sh
+++ command.sh
@@ -1,1 +1,2 @@
+export input='contract.pdf'
 docker run --rm -v /home/ubuntu/long-pdfs/pluribus.pdf:/input.pdf -v /home/ubuntu/long-pdfs/visualization.pdf:/visualization.pdf pdfact input.pdf --visualize visualization.pdf
